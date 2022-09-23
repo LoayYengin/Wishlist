84ff0d227803e235729f6a4b024d4774de459ae8
LoayYengin <yengin.loay@gmail.com>
2022-09-22 20:55:56 -0500

    implement user input and RV layout

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.wishlist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,19 +17,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Lookup the RecyclerView in activity layout
-        val wishesRv = findViewById<RecyclerView>(R.id.wishesRv)
+        // onBtnClick
+            // get the 3 text fields
+            // change wishFetcher to hold 3 lists instead of just name
+            // write function to insert into wishfetcher
 
-        // Fetch the list of emails
-        wishes = WishFetcher.getWishes()
+        val nameET = findViewById<EditText>(R.id.name_text)
+        val urlET = findViewById<EditText>(R.id.url_text)
+        val priceET = findViewById<EditText>(R.id.price_text)
+        val submitBtn = findViewById<Button>(R.id.submit_btn)
 
-        // Create adapter passing in the list of emails
-        val adapter = WishAdapter(wishes)
 
-        // Attach the adapter to the RecyclerView to populate items
-        wishesRv.adapter = adapter
+        submitBtn.setOnClickListener {
+            // Enter new wish item into the wishlist
+            WishFetcher.addWishes(nameET.text.toString(), urlET.text.toString(), priceET.text.toString())
 
-        // Set layout manager to position the items
-        wishesRv.layoutManager = LinearLayoutManager(this)
+            // Lookup the RecyclerView in activity layout
+            val wishesRv = findViewById<RecyclerView>(R.id.wishesRv)
+
+            // Fetch the list of emails
+            wishes = WishFetcher.getWishes()
+
+            // Create adapter passing in the list of emails
+            val adapter = WishAdapter(wishes)
+
+            // Attach the adapter to the RecyclerView to populate items
+            wishesRv.adapter = adapter
+
+            // Set layout manager to position the items
+            wishesRv.layoutManager = LinearLayoutManager(this)
+
+            nameET.text.clear()
+            urlET.text.clear()
+            priceET.text.clear()
+            hideKeyboard()
+        }
+
+    }
+
+    private fun hideKeyboard() {
+        val view = this.currentFocus
+
+        if (view != null) {
+            val hideMe = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hideMe.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
     }
 }
--- app/src/main/java/com/example/wishlist/WishFetcher.kt
+++ app/src/main/java/com/example/wishlist/WishFetcher.kt
@@ -2,18 +2,43 @@ package com.example.wishlist
 
 class WishFetcher {
     companion object {
+//        val name = listOf("Dahlia Cline", "Kevin Miranda")
         val name = listOf("Dahlia Cline", "Kevin Miranda", "Kaya Austin", "Laila Calderon", "Marquise Rhodes", "Fletcher Patel", "Luz Barron", "Kamren Dudley", "Jairo Foster", "Lilah Sandoval", "Ansley Blake", "Slade Sawyer", "Jaelyn Holmes", "Phoenix Bright", "Ernesto Gould")
-        val url = "URL input by user goes here!"
+//        val url = listOf("url1", "url2", "url3", "url4", "url5", "url6", "url7", "url8", "url9", "url1")
+//        val price = listOf("1.00", "2.00", "3.00", "4.00", "5.00", "6.00", "7.00", "8.00", "9.00", "1.00" )
+
+        val url = "url"
         val price = "123.00"
 
+        val nameList = mutableListOf<String>()
+        val urlList = mutableListOf<String>()
+        val priceList = mutableListOf<String>()
+
+        fun addWishes(name: String, url: String, price: String) {
+            nameList.add(name)
+            urlList.add(url)
+            priceList.add(price)
+
+        }
+
+        // update for loop range
         fun getWishes(): MutableList<Wish> {
             var wishes : MutableList<Wish> = ArrayList()
-            for (i in 0..9) {
-                val wish = Wish(name[i], url, price)
+            for (i in nameList.indices) {
+                val wish = Wish(nameList[i], urlList[i], priceList[i])
                 wishes.add(wish)
             }
             return wishes
         }
 
+//        // update for loop range
+//        fun getWishes(): MutableList<Wish> {
+//            var wishes : MutableList<Wish> = ArrayList()
+//            for (i in 0..wishCount) {
+//                val wish = Wish(nameList[i], urlList[i], priceList[i])
+//                wishes.add(wish)
+//            }
+//            return wishes
+//        }
     }
 }
